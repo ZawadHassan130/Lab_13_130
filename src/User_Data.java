9dd16a035fd4590a5441225f1a121b62be7b53e3
ZawadHassan130 <zawadhassan@iut-dhaka.edu>
2025-03-26 16:08:57 +0600

    This class contains every single customers  information

--- src/User_Data.java
+++ src/User_Data.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class User_Data {
+import java.util.ArrayList;
+import java.util.List;
+
+public class User_Data {
+
+    public static final List<Customer> customers = new ArrayList<Customer>();
 }
