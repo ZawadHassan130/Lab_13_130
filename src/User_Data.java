7eb8be2e61d481505d9dda6d559ae2ccbcd30837
ZawadHassan130 <zawadhassan@iut-dhaka.edu>
2025-03-26 23:59:32 +0600

    Additional Modification for newly added Admin class

--- src/User_Data.java
+++ src/User_Data.java
@@ -4,4 +4,12 @@ import java.util.List;
 public class User_Data {
 
     public static final List<Customer> customers = new ArrayList<Customer>();
+    public static  final List<Admin> admins = new ArrayList<>();
+
+    static {
+
+        if(admins.isEmpty())
+            admins.add(new Admin("root","root"));
+    }
+
 }
